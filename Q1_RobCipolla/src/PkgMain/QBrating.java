d207271f254879a66695c6161a30fec6bdf205ef
robertcipolla <robertcipolla@roberts-mbp.fios-router.home>
2017-09-10 21:08:49 -0400

    q1

---
+++ Q1_RobCipolla/src/PkgMain/QBrating.java
@@ -0,0 +1,41 @@
+package PkgMain;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class QBrating {
+	public void QB_rating() {
+		
+		Scanner qbinfo = new Scanner(System.in);
+		
+		System.out.println("Enter number of passing attempts: ");
+		double ATT = qbinfo.nextInt();
+
+		System.out.println("Enter number of completions: " );
+		double COMP = qbinfo.nextInt();
+		
+		System.out.println("Enter number of passing yards: " );
+		double YDS = qbinfo.nextInt();
+		
+		System.out.println("Enter number of touchdown passes: " );
+		double TD = qbinfo.nextInt();
+
+		System.out.println("Enter number of interceptions: " );
+		double INT = qbinfo.nextInt();
+		
+		double a = ((COMP/ATT) - .3) * 5;
+		double b = ((YDS/ATT) - 3) * .25;
+		double c = (TD/ATT) * 20;
+		double d = 2.375 - ((INT/ATT) * 25);
+		
+		double rating = ((a+b+c+d) / 6) * 100;
+		DecimalFormat numberFormat = new DecimalFormat("#.0");
+		
+		if ((COMP/ATT)>=.775 && (YDS/ATT)>=12.5 && (TD/ATT)>=.11875 && INT == 0) {
+			rating = 158.3;
+		}
+		
+		System.out.println("The QB rating is " + numberFormat.format(rating));
+		
+	}
+}
---
+++ Q1_RobCipolla/src/PkgTest/QB_Rating_Test.java
@@ -0,0 +1,16 @@
+package PkgTest;
+
+import org.junit.Test;
+
+import PkgMain.QBrating;
+
+public class QB_Rating_Test {
+		
+		@Test
+		public void TestRating() {
+			QBrating qbtest = new QBrating();
+			qbtest.QB_rating();
+			
+		}
+		
+}
